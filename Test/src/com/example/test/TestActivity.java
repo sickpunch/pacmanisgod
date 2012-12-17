c71c3cb39f68164a3b8c34d15b5cc5855d5e9047
sickpunch <sickpunches@gmail.com>
2012-12-17 22:19:58 +0200

    This is a test.

---
+++ Test/src/com/example/test/TestActivity.java
@@ -0,0 +1,31 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        
+        Context context = getApplicationContext();
+        CharSequence text = "DEEEEEEBISH!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_test, menu);
+        return true;
+    }
+    
+}
